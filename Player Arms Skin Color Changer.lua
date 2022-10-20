505533094318adeeb4ae745bd1510479911568a7
Exodouding <76268043+Exodouding@users.noreply.github.com>
2022-10-21 00:44:55 +0200

    Update Player Arms Skin Color Changer.lua

--- Player Arms Skin Color Changer.lua
+++ Player Arms Skin Color Changer.lua
@@ -1,6 +1,7 @@
--- Made By Exodouding for skeet.cc/gamesense.pub
+-- Player Arms Skin Color Changer, Made By Exodouding for skeet.cc/gamesense.pub
 
-local skin_colors = {
+local skin_colors =
+{
     "White",
     "Black",
     "Brown",
@@ -9,62 +10,56 @@ local skin_colors = {
     "Tattoo"
 }
 
-local LastSelectedIndex = -1
-
 local no_sleeve = ui.new_checkbox("Skins", "Model Options", "No Sleeve")
+local player_skin_color = ui.new_checkbox("Skins", "Model Options", "Player Skin Color")
+local skins = ui.new_combobox("Skins", "Model Options", "\n", skin_colors)
 
-local PlayerSkinColor = ui.new_checkbox("Skins", "Model Options", "Player Skin Color")
-local Skins = ui.new_combobox("Skins", "Model Options", "\n", skin_colors)
-ui.set_visible(Skins, false)
-
-ui.set_callback(PlayerSkinColor, function()
-
-	local Result = ui.get(PlayerSkinColor)
-	
-	ui.set_visible(Skins, Result)
-	
-	if not Result then 
-		client.set_cvar("r_skin", 0)
-		LastSelectedIndex = -1
-	end
-	
-end)
+ui.set_visible(skins, false)
 
 local function NoSleeve()
 
-    local sleeve = materialsystem.find_materials("sleeve")
+    local materials = materialsystem.find_materials("sleeve")
 	
-    for i=#sleeve, 1, -1 do
-	
-        if ui.get(no_sleeve) then
-            sleeve[i]:set_material_var_flag(2, true)
-        else
-            sleeve[i]:set_material_var_flag(2, false)
+	if materials == nil then
+        return
+    end
+    
+    for _, material in pairs(materials) do
+
+        if material ~= nil then
+
+            material:set_material_var_flag(2, ui.get(no_sleeve))
         end
     end
 end
 
-local function SkinColor()
+local function Skins()
 
-	if not ui.get(PlayerSkinColor) then 
-		return
-	end
-	
 	for i, v in pairs(skin_colors) do
 		
-		local SelectedSkin = ui.get(Skins, skin_colors)
+		local SelectedSkin = ui.get(skins, skin_colors)
 		
-		if SelectedSkin == skin_colors[i] and i ~= LastSelectedIndex then
-			client.set_cvar("r_skin", i-1)
+		if SelectedSkin == skin_colors[i] then
+
+			client.set_cvar("r_skin", i - 1)
 			-- print("Name : " .. v .. " | Skin ID : " .. i)
-			LastSelectedIndex = i
 		end
 	end
 end
 
-local function Main()
-	NoSleeve()
-	SkinColor()
+local function PlayerSkinColor()
+
+	local result = ui.get(player_skin_color)
+	
+	ui.set_visible(skins, result)
+	
+	if not result then
+		client.set_cvar("r_skin", 0)
+    else 
+        Skins()
+	end
 end
 
-client.set_event_callback("paint", Main)
+ui.set_callback(no_sleeve, NoSleeve)
+ui.set_callback(player_skin_color, PlayerSkinColor)
+ui.set_callback(skins, Skins)

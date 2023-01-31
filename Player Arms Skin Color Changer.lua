6751c374ed23591f1c83a876ed7077b566ebf205
Exodouding <76268043+Exodouding@users.noreply.github.com>
2023-01-31 17:39:07 +0100

    Update Player Arms Skin Color Changer.lua

--- Player Arms Skin Color Changer.lua
+++ Player Arms Skin Color Changer.lua
@@ -11,10 +11,26 @@ local skin_colors =
 }
 
 local no_sleeve = ui.new_checkbox("Skins", "Model Options", "No Sleeve")
+--local no_sleeve_oppacity = ui.new_slider("Skins", "Model Options", "No Sleeve Oppcatiy", 0, 100, 100, true, "%", 1)
 local player_skin_color = ui.new_checkbox("Skins", "Model Options", "Player Skin Color")
-local skins = ui.new_combobox("Skins", "Model Options", "\n", skin_colors)
+local skins_ct = ui.new_combobox("Skins", "Model Options", "CT Skin", skin_colors)
+local skins_t = ui.new_combobox("Skins", "Model Options", "T Skin", skin_colors)
 
-ui.set_visible(skins, false)
+ui.set_visible(skins_ct, false)
+ui.set_visible(skins_t, false)
+
+local function IsInGame()
+
+	local local_player = entity.get_local_player()
+
+	local team = entity.get_prop(local_player, "m_iTeamNum")
+
+	if team == 0 or team == 1 or team == 2 or team == 3 then -- Player Is In Game
+		return 1
+	else -- Player Is In Main Menu
+		return 0
+	end
+end
 
 local function NoSleeve()
 
@@ -31,18 +47,74 @@ local function NoSleeve()
             material:set_material_var_flag(2, ui.get(no_sleeve))
         end
     end
+
+	if ui.get(no_sleeve) then
+
+        return
+    end
 end
 
 local function Skins()
 
-	for i, v in pairs(skin_colors) do
-		
-		local SelectedSkin = ui.get(skins, skin_colors)
-		
-		if SelectedSkin == skin_colors[i] then
+	local result = ui.get(player_skin_color)
 
-			client.set_cvar("r_skin", i - 1)
+	for i, v in pairs(skin_colors) do
+
+		local local_player = entity.get_local_player()
+	
+		local team = entity.get_prop(local_player, "m_iTeamNum")
+	
+		if team == 2 and result then -- Player is in the terrorist team and checkbox is on
+
+			ui.set_visible(skins_t, true)
+			ui.set_visible(skins_ct, false)
+
+			local SelectedSkinT = ui.get(skins_t, skin_colors)
+
+			if SelectedSkinT == skin_colors[i] then
+				client.set_cvar("r_skin", i - 1)
+			end
 			-- print("Name : " .. v .. " | Skin ID : " .. i)
+		
+		elseif team == 2 and not result then -- Player is in the terrorist team and checkbox isn't on
+
+			ui.set_visible(skins_t, false)
+
+		elseif team == 3 and result then -- Player is in the counter-terrorist team and checkbox is on
+
+			ui.set_visible(skins_ct, true)
+			ui.set_visible(skins_t, false)
+
+			local SelectedSkinCT = ui.get(skins_ct, skin_colors)
+
+			if SelectedSkinCT == skin_colors[i] then
+				client.set_cvar("r_skin", i - 1)
+			end
+			-- print("Name : " .. v .. " | Skin ID : " .. i)
+			
+		elseif team == 3 and not result then -- Player is in the counter-terrorist team and checkbox isn't on
+		
+			ui.set_visible(skins_ct, false)
+		
+		elseif IsInGame() == 0 and result then -- Player is in main menu and checkbox is on
+		
+			ui.set_visible(skins_t, true)
+			ui.set_visible(skins_ct, true)
+			
+		elseif IsInGame() == 0 and not result then -- Player is in main menu and checkbox isn't on
+		
+			ui.set_visible(skins_t, false)
+			ui.set_visible(skins_ct, false)
+			
+		elseif (team == 0 or team == 1) and result then -- Player is spectating or didn't chose a team and checkbox is on
+		
+			ui.set_visible(skins_t, true)
+			ui.set_visible(skins_ct, true)
+			
+		elseif (team == 0 or team == 1) and not result then -- Player is spectating or didn't chose a team and checkbox isn't on
+		
+			ui.set_visible(skins_t, false)
+			ui.set_visible(skins_ct, false)
 		end
 	end
 end
@@ -51,8 +123,6 @@ local function PlayerSkinColor()
 
 	local result = ui.get(player_skin_color)
 	
-	ui.set_visible(skins, result)
-	
 	if not result then
 		client.set_cvar("r_skin", 0)
     else 
@@ -60,6 +130,6 @@ local function PlayerSkinColor()
 	end
 end
 
-ui.set_callback(no_sleeve, NoSleeve)
+client.set_event_callback("paint", NoSleeve)
 ui.set_callback(player_skin_color, PlayerSkinColor)
-ui.set_callback(skins, Skins)
+client.set_event_callback("paint_ui", Skins)

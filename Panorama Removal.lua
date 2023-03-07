e14aec3c780076a841da22bd17ca0eb3d063f726
Exodouding <76268043+Exodouding@users.noreply.github.com>
2023-03-07 17:51:57 +0100

    Update Panorama Removal.lua

--- Panorama Removal.lua
+++ Panorama Removal.lua
@@ -1,10 +1,12 @@
 -- Panorama Removal, Made By Exodouding for skeet.cc/gamesense.pub
 
-local panorama_buttons_names = {}
+local panorama_table_buttons_game = {}
 
-local panorama_elements_names = {}
+local panorama_table_buttons_menu = {}
 
-local panorama_buttons = 
+local panorama_elements = {}
+
+local panorama_buttons_menu = 
 {
 	{ name = "Dashboard", value = "MainMenuNavBarHome" },
 	{ name = "Play", value = "MainMenuNavBarPlay" },
@@ -14,43 +16,77 @@ local panorama_buttons =
 	{ name = "Overwatch", value = "MainMenuNavBarOverwatch" },
 	{ name = "Overwatch Notification", value = "MainMenuOverwatchAlert" },
 	{ name = "Settings", value = "MainMenuNavBarSettings" },
-	{ name = "Settings Notification", value = "MainMenuSettingsAlert" }
-	--[[
+	{ name = "Settings Notification", value = "MainMenuSettingsAlert" },
 	{ name = "Quit", value = "MainMenuNavBarQuit" },
 	{ name = "Disconnect Server", value = "MainMenuNavBarExitGame" },
 	{ name = "Banner Remover", value = "NotificationsContainer" },
-
 	{ name = "Resume Game", value = "MainMenuNavBarResumeGame" },
 	{ name = "Switch Teams", value = "MainMenuNavBarSwitchTeams" },
 	{ name = "Vote", value = "MainMenuNavBarVote" },
 	{ name = "Report Server", value = "MainMenuNavBarReportServer" },
 	{ name = "Community Server Browser", value = "MainMenuNavBarShowCommunityServerBrowser" }
-	]]
+}
+
+local panorama_buttons_game = 
+{
+	{ name = "Dashboard", value = "MainMenuNavBarHome" },
+	{ name = "Play", value = "MainMenuNavBarPlay" },
+	{ name = "Inventory", value = "MainMenuNavBarInventory" },
+	{ name = "Watch", value = "MainMenuNavBarWatch" },
+	{ name = "Stats Panel", value = "MainMenuNavBarStats" },
+	{ name = "Overwatch", value = "MainMenuNavBarOverwatch" },
+	{ name = "Overwatch Notification", value = "MainMenuOverwatchAlert" },
+	{ name = "Settings", value = "MainMenuNavBarSettings" },
+	{ name = "Settings Notification", value = "MainMenuSettingsAlert" },
+	{ name = "Quit", value = "MainMenuNavBarQuit" },
+	{ name = "Disconnect Server", value = "MainMenuNavBarExitGame" },
+	{ name = "Banner Remover", value = "NotificationsContainer" },
+	{ name = "Resume Game", value = "MainMenuNavBarResumeGame" },
+	{ name = "Switch Teams", value = "MainMenuNavBarSwitchTeams" },
+	{ name = "Vote", value = "MainMenuNavBarVote" },
+	{ name = "Report Server", value = "MainMenuNavBarReportServer" },
+	{ name = "Community Server Browser", value = "MainMenuNavBarShowCommunityServerBrowser" }
 }
 
 local panorama_background = 
 {
 	{ name = "Hide News Panel", value = "JsNewsContainer" },
-    	{ name = "Hide News Panel Content", value = "NewsPanelLister" },
+    { name = "Hide News Panel Content", value = "NewsPanelLister" },
 	{ name = "Hide Player Model", value = "JsMainmenu_Vanity" },
-    	{ name = "Hide Player Model + Wheel", value = "JsMainmenu_Vanity-Container" },
+    { name = "Hide Player Model + Wheel", value = "JsMainmenu_Vanity-Container" },
 	{ name = "Dark Panorama", value = "MainMenuBackground" },
 	{ name = "Dark Panorama With Player", value = "MainMenuMovie" },
 	{ name = "Hide Right Panel", value = "MainMenuFriendsAndParty" },
 	{ name = "Hide Left Panel", value = "MainMenuNavBarLeft" }
 }
 
-for _, element in pairs(panorama_buttons) do
-	table.insert(panorama_buttons_names, element.name)
+for _, element in pairs(panorama_buttons_game) do
+	table.insert(panorama_table_buttons_game, element.name)
+end
+
+for _, element in pairs(panorama_buttons_menu) do
+	table.insert(panorama_table_buttons_menu, element.name)
 end
 
 for _, element in pairs(panorama_background) do
-	table.insert(panorama_elements_names, element.name)
+	table.insert(panorama_elements, element.name)
+end
+
+local function IsInGame()
+
+	local local_player = entity.get_local_player()
+
+	local team = entity.get_prop(local_player, "m_iTeamNum")
+	
+	if team ~= nil then	
+		return true -- Player Is In Game
+	else 
+		return false -- Player Is In Main Menu
+	end
 end
 
 local mainmenueditor = panorama.loadstring(
 [[
-
 return {
 	set: (str, toggle) => {
 		var element = $.GetContextPanel().FindChildTraverse(str);
@@ -60,52 +96,92 @@ return {
 		}
 	}
 }
-
 ]], "MainMenu")()
 
-local panorama_blur = ui.new_checkbox("MISC", "Miscellaneous", "Panorama Blur")
+local panorama_blur = ui.new_checkbox("Misc", "Miscellaneous", "Panorama Blur")
 
-local panorama_multiselect = ui.new_multiselect("MISC", "Miscellaneous", "Panorama Buttons", panorama_buttons_names)
+local panorama_box_shadow = ui.new_checkbox("Misc", "Miscellaneous", "Panorama Box Shadow")
 
-local panorama_multiselect_v2 = ui.new_multiselect("MISC", "Miscellaneous", "Panorama Background", panorama_elements_names)
+local panorama_multiselect_game = ui.new_multiselect("Misc", "Miscellaneous", "In Game Panorama Buttons", panorama_table_buttons_game)
 
-local function Contains(_table, _element)
+local panorama_multiselect_menu = ui.new_multiselect("Misc", "Miscellaneous", "Main Menu Panorama Buttons", panorama_table_buttons_menu)
 
-	for _, value in pairs(_table) do
-	
-		if value == _element then
-			return true
-		end
+local panorama_multiselect_panorama = ui.new_multiselect("Misc", "Miscellaneous", "Panorama Background", panorama_elements)
+
+local function Contains(table, value)
+	if table == nil then
+		return false
 	end
-	
-	return false
+    
+    if type(table) == "number" then
+        table = ui.get(table)
+    end
+
+    for i=0, #table do
+        if table[i] == value then
+            return true
+        end
+    end
+
+    return false
 end
 
 local function PanoramaButtons()
 
-	local SelectedPanoramas = ui.get(panorama_multiselect)
+	local SelectedPanoramasGame = ui.get(panorama_multiselect_game)
 	
-	for _, element in pairs(panorama_buttons) do
+	local SelectedPanoramasMenu = ui.get(panorama_multiselect_menu)
 	
-		if Contains(SelectedPanoramas, element.name) then
-			mainmenueditor.set(element.value, false)
-		else
-			mainmenueditor.set(element.value, true)
+	if IsInGame() then 
+	
+		ui.set_visible(panorama_multiselect_game, true)
+		ui.set_visible(panorama_multiselect_menu, false)
+	
+		for _, element in pairs(panorama_buttons_menu) do
+		
+			if Contains(SelectedPanoramasGame, element.name) then
+				mainmenueditor.set(element.value, false)
+			else
+				mainmenueditor.set(element.value, true)
+			end
+		end
+		
+	else
+	
+		ui.set_visible(panorama_multiselect_game, false)
+		ui.set_visible(panorama_multiselect_menu, true)
+
+		for _, element in pairs(panorama_buttons_game) do
+		
+			if Contains(SelectedPanoramasMenu, element.name) then
+				mainmenueditor.set(element.value, false)
+			else
+				mainmenueditor.set(element.value, true)
+			end
 		end
 	end
 end
 
 local function PanoramaBackground()
 
-	local SelectedPanoramas = ui.get(panorama_multiselect_v2)
+	local SelectedPanoramas = ui.get(panorama_multiselect_panorama)
 	
-	for _, element in pairs(panorama_background) do
+	if not IsInGame() then
 	
-		if Contains(SelectedPanoramas, element.name) then
-			mainmenueditor.set(element.value, false)
-		else
-			mainmenueditor.set(element.value, true)
+		ui.set_visible(panorama_multiselect_panorama, true)
+	
+		for _, element in pairs(panorama_background) do
+		
+			if Contains(SelectedPanoramas, element.name) then
+				mainmenueditor.set(element.value, false)
+			else
+				mainmenueditor.set(element.value, true)
+			end
 		end
+		
+	else
+		ui.set_visible(panorama_multiselect_panorama, false)
+		return
 	end
 end
 
@@ -113,6 +189,11 @@ local function PanoramaBlur()
 	client.set_cvar("@panorama_disable_blur", ui.get(panorama_blur))
 end
 
-ui.set_callback(panorama_multiselect_v2, PanoramaBackground)
-ui.set_callback(panorama_multiselect, PanoramaButtons)
+local function PanoramaBoxShadow()
+	client.set_cvar("@panorama_disable_box_shadow", ui.get(panorama_box_shadow))
+end
+
+client.set_event_callback("paint_ui", PanoramaBackground)
+client.set_event_callback("paint_ui", PanoramaButtons)
 ui.set_callback(panorama_blur, PanoramaBlur)
+ui.set_callback(panorama_box_shadow, PanoramaBoxShadow)
